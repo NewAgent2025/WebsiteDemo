2381edaf0d585db7c3195c83b9ec2dd85f73847e
NewAgent’s Demon <David2314001@proton.me>
2025-11-19 17:47:22 -0800

    Update sw.js

--- Notifcations/sw.js
+++ Notifcations/sw.js
@@ -1,2 +1,7 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => clients.claim());
+self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => clients.claim());
+
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow("/"));
+});
